8850d1441dd7ec96ea10ec726812eb8ed7398312
Julien Wolflisberg <julien.wolflisberg@he-arc.ch>
2011-10-25 10:47:48 +0200

    Project initialized ! Just a "rails new MyFM -d mysql" Signed-off-by: Julien Wolflisberg <julien.wolflisberg@he-arc.ch>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyFM::Application.config.secret_token = '3264c1833cc8528d4400a752a2f7cf6e2da398258239a36ee106a8611b96a5c2cee9be282c0abb7e71d3e360b031ca42427c004bbbc39ae70f5409ed9d693c2b'
