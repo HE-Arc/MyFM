3f4502eea8bdc5ee3328ecd9be8cf3e05cbc69e4
Alexis Cramatte <alexis.cramatte@gmail.com>
2011-11-09 16:07:20 +0100

    Fixed clearance (for real this time)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,7 @@ MyFM::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  # config/environments/{development,test}.rb
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,4 +36,7 @@ MyFM::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+  
+  # config/environments/{development,test}.rb
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
