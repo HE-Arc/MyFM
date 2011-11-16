9cab048b8b9a250d696f0303da2be8d744b151c8
Anthony Geiser <antho.geiser@gmail.com>
2011-11-16 14:02:54 +0100

    Conflict version clear

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,5 +1,4 @@
 class Band < ActiveRecord::Base
-<<<<<<< HEAD
   
   after_initialize :default_values
   
@@ -9,7 +8,6 @@ class Band < ActiveRecord::Base
     self.name = "Band"
   end
   
-=======
   has_many :posts, :order => "created_at DESC"
->>>>>>> 413b83724a5ee2606f1dbadc68d22b6e4594e72c
+  
 end
