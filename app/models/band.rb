0a8ddc22af07c8ea4b06c9a0d62dbbe4ddb14891
Alexis Cramatte <alexis.cramatte@gmail.com>
2011-11-22 06:39:57 -0800

    Length validation for the band name attribute (max 30) + test

--- app/models/band.rb
+++ app/models/band.rb
@@ -2,6 +2,7 @@ class Band < ActiveRecord::Base
   
   has_many :posts, :order => "created_at DESC"
   
-  validates :name, :presence => true
+  validates :name, :presence => true,
+                   :length   => { :maximum => 30 }
   
 end
--- test/unit/band_test.rb
+++ test/unit/band_test.rb
@@ -7,4 +7,10 @@ class BandTest < ActiveSupport::TestCase
     assert !band.save, "Saved the band without a name"
   end
   
+  test "should not save band with a too long name" do
+    long_name = "a" * 31
+    long_name_band = Band.new(:name=> long_name)
+    assert !long_name_band.save, "Saved the band with a name too long"
+  end
+  
 end
