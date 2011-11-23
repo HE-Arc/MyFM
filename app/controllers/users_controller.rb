500b6e6fd291a97c0b79520563e604b1fe881f3b
Anthony Geiser <antho.geiser@gmail.com>
2011-11-23 05:52:48 -0800

    User controller fixed.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < Clearance::UsersController
-  before_filter :authorize
+  
+  #before_filter :authorize
+  
   def show
-    #@user = current_user.id
+    @user = User.find(params[:id])
   end
   
   # Sign up
   def url_after_create
     user_path(current_user.id)
   end
+  
 end
