a179b3d90247e0794e94a9ea0f191f9360b52262
Anthony Geiser <antho.geiser@gmail.com>
2011-11-24 00:31:45 -0800

    Add title helper in application_helper. Add pages_helper (generated)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Return a title based by the page
+  def title
+    base_title = "MyFM"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+
 end
