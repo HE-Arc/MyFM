b214a666e71ba94d1f2e1b80416ccb8c63f7714c
Anthony Geiser <antho.geiser@gmail.com>
2011-11-24 00:39:52 -0800

    Pages controller integrated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 MyFM::Application.routes.draw do
   
+  get "pages/home"
+  get "pages/about"
+  
   resources :bands do
     resources :posts
   end
@@ -11,9 +14,6 @@ MyFM::Application.routes.draw do
   match '/sign_out' => 'sessions#destroy'
   
   resources :users
-
-  get "home/index"
-  
   
 
   # The priority is based upon order of creation:
@@ -65,7 +65,7 @@ MyFM::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
