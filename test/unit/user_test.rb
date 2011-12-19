74e558851daba9ca54d405a51ff64756e4d97e8e
acramatte <alexis.cramatte@gmail.com>
2011-12-19 21:22:25 +0100

    added users methods tests and some fixtures

---
+++ test/unit/user_test.rb
@@ -0,0 +1,59 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  fixtures :users, :bands
+
+  ###########Tests isInBand###########
+  ####################################
+  test "users should have a isInBand method" do
+    user = users(:user1)
+    assert_respond_to( user, "isInBand")
+  end
+
+  ###########Tests joinband###########
+  ####################################
+  test "users should have a joinband method" do
+    user = users(:user1)
+    assert_respond_to( user, "joinband")
+  end
+
+  test "user1 should be in band2" do
+    user = users(:user1)
+    band = bands(:band2)
+    user.joinband(band)
+    assert(user.isInBand(band))
+  end
+
+  test "user1 should not be in band2" do
+    user = users(:user1)
+    band = bands(:band2)
+    assert_nil(user.isInBand(band))
+  end
+
+  ###########Tests leaveband###########
+  ####################################
+  test "users should have a leaveband method" do
+    user = users(:user1)
+    assert_respond_to( user, "leaveband")
+  end
+
+  test "user1 should not be in band2 anymore" do
+    user = users(:user1)
+    band = bands(:band2)
+    user.joinband(band)
+    assert_not_nil(user.isInBand(band))
+    user.leaveband(band)
+    assert_nil(user.isInBand(band))
+  end
+  
+  ###########Tests mybands###########
+  ####################################
+  test "user1 should have 1 band" do
+    user = users(:user1)
+    band = bands(:band1)
+    user.joinband(band)
+    assert_equal(user.bandrelations.find_all_by_band_id(band),user.mybands())
+  end
+  
+  
+end
